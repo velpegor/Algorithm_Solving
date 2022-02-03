23057252d221646e904ef73184c6436a18748371
Wonjun <velpegor@naver.com>
2022-02-04 03:21:36 +0900

    BOJ 1158

---
+++ Python/1158.py
@@ -0,0 +1,13 @@
+n, k = map(int,input().split())
+arr = [i for i in range(1, n+1)]
+
+kill_arr = []
+position = 0
+
+for i in range(n):
+    position += k-1
+    if position >= len(arr):
+        position = position % len(arr)
+    kill_arr.append(arr.pop(position))
+
+print("<",', '.join(str(i) for i in kill_arr), '>', sep="")
