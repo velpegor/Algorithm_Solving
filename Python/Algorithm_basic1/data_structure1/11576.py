ff9244fe4b473443f7d667beff7f21f18edd7198
Wonjun <velpegor@naver.com>
2022-03-24 02:01:37 +0900

    BOJ 11576

---
+++ Python/Algorithm_basic1/data_structure1/11576.py
@@ -0,0 +1,15 @@
+A, B = map(int, input().split())
+n = int(input())
+a = list(map(int, input().split()))
+num = 0
+answer= []
+
+for i in range(n):
+    num += a[-1] * (A**i)
+    a.pop(-1)
+
+while num != 0:
+    answer.append(str(num % B))
+    num //= B
+
+print(" ".join(answer[::-1]))
