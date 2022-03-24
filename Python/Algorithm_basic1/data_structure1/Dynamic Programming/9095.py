50d3639c884dddf1c6ad237e08e0425f27df7033
velpegor <velpegor@naver.com>
2022-03-25 02:43:54 +0900

    BOJ 9095

---
+++ Python/Algorithm_basic1/data_structure1/Dynamic Programming/9095.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+def sol(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return sol(n-1) + sol(n-2) + sol(n-3)
+
+for i in range(n):
+    num = int(input())
+    print(sol(num))
